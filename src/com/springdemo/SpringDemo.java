827f0426f0f845ca00729bd935d9554f84a5d362
thevansh <vansh.jain@zemosolabs.com>
2021-07-21 15:33:37 +0530

    implemented bean scope singleton and prototype

--- src/com/springdemo/SpringDemo.java
+++ src/com/springdemo/SpringDemo.java
@@ -8,9 +8,11 @@ public class SpringDemo {
         ClassPathXmlApplicationContext context=
                 new ClassPathXmlApplicationContext("applicationContext.xml");
 
-        Animal animal=context.getBean("theLion",Animal.class);
-        animal.makeSound();
-        animal.getNumberOfLegs();
-
+        Animal animal1=context.getBean("theLion",Animal.class);
+        Animal animal2=context.getBean("theLion",Animal.class);
+        animal1.makeSound();
+        animal1.getNumberOfLegs();
+        if(animal1==animal2)
+            System.out.println("Same object");
     }
 }
