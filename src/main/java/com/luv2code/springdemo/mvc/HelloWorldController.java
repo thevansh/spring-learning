b3a2c30f9893c81bc4ccbd840854a4af23781681
thevansh <vansh.jain@zemosolabs.com>
2021-07-22 22:50:49 +0530

    refactor: replace HttpServletRequest with RequestParm annotaion

--- src/main/java/com/luv2code/springdemo/mvc/HelloWorldController.java
+++ src/main/java/com/luv2code/springdemo/mvc/HelloWorldController.java
@@ -3,6 +3,7 @@ package com.luv2code.springdemo.mvc;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.Locale;
@@ -24,6 +25,13 @@ public class HelloWorldController {
         public String convertCase(HttpServletRequest request, Model model){
             String message=request.getParameter("studentName");
             message="Hello " +message.toUpperCase();
+            model.addAttribute("message",message);
+            return "helloworld";
+        }
+
+        @RequestMapping("processFormVersion3")
+        public String processFormVersion3(@RequestParam("studentName") String message, Model model){
+            message="Hello " +message.toUpperCase()+", Welcome to v3";
             model.addAttribute("message",message);
             return "helloworld";
         }
--- src/main/webapp/WEB-INF/view/helloworld-form.jsp
+++ src/main/webapp/WEB-INF/view/helloworld-form.jsp
@@ -5,7 +5,7 @@
     <title>Hello Form</title>
 </head>
 <body>
-<form action="processFormVersion2" method="GET">
+<form action="processFormVersion3" method="GET">
   <input type="text" name="studentName" placeholder="Enter your Name" />
   <input type="submit" />
 </form>
