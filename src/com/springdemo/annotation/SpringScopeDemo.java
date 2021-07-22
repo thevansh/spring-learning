393a44cb5a92725f1687a8b1e3461f956e09686e
thevansh <vansh.jain@zemosolabs.com>
2021-07-22 09:30:49 +0530

    refactor: added impl of scope via annotation

---
+++ src/com/springdemo/annotation/SpringScopeDemo.java
@@ -0,0 +1,16 @@
+package com.springdemo.annotation;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class SpringScopeDemo {
+    public static void main(String[] args) {
+        ClassPathXmlApplicationContext context=
+                new ClassPathXmlApplicationContext("applicationContext.xml");
+        Furniture type1=context.getBean("chairSet",Furniture.class);
+        Furniture type2=context.getBean("chairSet",Furniture.class);
+        if(type1==type2)
+            System.out.println("singleton");
+        else
+            System.out.println("Prototype");
+    }
+}
